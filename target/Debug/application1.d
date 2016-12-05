c66b94709ba1921b2be4c9df2ffb1c3ba9896725
abombard <arthbab@gmail.com>
2016-12-05 13:03:23 +0100

    cleanup

--- target/Debug/application1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Teabagg\Visual Studio 2015\Projects\ParticleSystem\rust_application1\target\debug\application1.exe: src\main.rs
-
-C:\Users\Teabagg\Visual Studio 2015\Projects\ParticleSystem\rust_application1\target\debug\application1.d: src\main.rs
-
-src\main.rs:
